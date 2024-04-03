9d1815d597150b56efd24650019202d3aea45215
juanbiantong <jbiantong@gmail.com>
2024-04-03 14:45:36 +0700

    first commit

---
+++ database.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+// Get database connection string from environment variables
+const connectionString = process.env.DB_CONNECTION_STRING;
+
+// Create a new PostgreSQL Pool instance
+const pool = new Pool({
+ connectionString: connectionString,
+});
+
+// Test the database connection
+async function connection() {
+ const connect = await pool.query('SELECT NOW()', (err, res) => {
+
+  if (err) {
+   console.log('Error connecting to the database:', err);
+  } else {
+   return ('Connected to the database at:', res.rows[0].now);
+  }
+
+ });
+}
+
+module.exports = { connection };
---
+++ server.js
@@ -0,0 +1,19 @@
+// Import required modules
+const express = require('express');
+const { connection } = require('./database');
+
+// Create Express app
+const app = express();
+
+// Define routes and other middleware as needed
+// Example route
+app.get('/', async (req, res) => {
+ const connetionState = await connection()
+ res.send(connetionState);
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+ console.log(`Server is running on port http://localhost:${PORT}`);
+});
